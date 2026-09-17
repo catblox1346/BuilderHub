19b17dc1c63ae7bee29d59532f45ab11df0f7438
Basket <111329237+catblox1346@users.noreply.github.com>
2026-09-16 18:32:29 -0700

    Reverted | Old Url

--- Loader.lua
+++ Loader.lua
@@ -2,4 +2,4 @@ repeat
 	task.wait()
 until game:IsLoaded()
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7b1f1cadd44f51e4958bbb9f30c89568.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/b6711b16b0770f1e2a96659dbf64832b.lua"))()
